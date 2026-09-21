d408d56ec194750fbbd8fcd630dbc1a053c05215
trungythp1-jpg <trungythp1@gmail.com>
2026-09-21 17:33:55 +0700

    Update index.js

--- functions/src/index.js
+++ functions/src/index.js
@@ -3,8 +3,18 @@ const { onRequest } = require('firebase-functions/v2/https');
 require('./lib/admin');
 
 const { bootstrapAdmin } = require('./auth/bootstrap-admin');
+const {
+  createWorkOrder,
+  completeWorkOrder,
+  closeWorkOrder
+} = require('./work-orders');
 
-exports.bootstrapAdmin = bootstrapAdmin;
+const {
+  requestConfirmation,
+  finalizeConfirmation
+} = require('./confirmations');
+
+const { processImport } = require('./imports');
 
 exports.health = onRequest((req, res) => {
   res.status(200).json({
@@ -14,3 +24,14 @@ exports.health = onRequest((req, res) => {
     version: 'V1.0'
   });
 });
+
+exports.bootstrapAdmin = bootstrapAdmin;
+
+exports.createWorkOrder = createWorkOrder;
+exports.completeWorkOrder = completeWorkOrder;
+exports.closeWorkOrder = closeWorkOrder;
+
+exports.requestConfirmation = requestConfirmation;
+exports.finalizeConfirmation = finalizeConfirmation;
+
+exports.processImport = processImport;
