533a7c63aac5a9d5fa74bbe4e18fa21508752c83
trungythp1-jpg <trungythp1@gmail.com>
2026-09-21 15:30:27 +0700

    Update elevator.js

--- functions/src/validation/elevator.js
+++ functions/src/validation/elevator.js
@@ -1,7 +1,33 @@
-const { clean, normalizeText } = require('../lib/admin');
+const { clean, normalizeText } = require('../lib/utils');
 const { assertRequired } = require('./common');
+
 function validateElevator(data) {
-  assertRequired(data, ['buildingId','name']);
-  return {...data, buildingId: clean(data.buildingId), name: clean(data.name), nameNormalized: normalizeText(data.name), status: data.status || 'ACTIVE', serviceStatus: data.serviceStatus || 'NOT_MANAGED'};
+  assertRequired(data, ['buildingId', 'name']);
+
+  const buildingId = clean(data.buildingId);
+  const name = clean(data.name);
+  const serialNumber = clean(data.serialNumber);
+
+  return {
+    ...data,
+    buildingId,
+    name,
+    nameNormalized: normalizeText(name),
+
+    serialNumber,
+    serialNumberNormalized: serialNumber
+      ? normalizeText(serialNumber)
+      : '',
+
+    manufacturer: clean(data.manufacturer),
+    machineBrand: clean(data.machineBrand),
+    machineModel: clean(data.machineModel),
+    controllerBrand: clean(data.controllerBrand),
+    controllerModel: clean(data.controllerModel),
+
+    status: data.status || 'ACTIVE',
+    serviceStatus: data.serviceStatus || 'NOT_MANAGED'
+  };
 }
+
 module.exports = { validateElevator };
