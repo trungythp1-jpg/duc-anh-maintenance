009787ed8383a2b162647f1b8639332bca824c57
trungythp1-jpg <trungythp1@gmail.com>
2026-09-21 17:31:07 +0700

    Update generate.js

--- functions/src/pdf/generate.js
+++ functions/src/pdf/generate.js
@@ -1,8 +1,34 @@
-const { db, now, requireAuth, assertRole } = require('../lib/admin');
-async function createDocumentRecord({type,recordId,storagePath,url=null,auth}){
- assertRole(auth,['admin','manager','customer_service']);
- const ref=db.collection('generatedDocuments').doc();
- await ref.set({type,recordId,storagePath:storagePath||null,url,createdBy:auth.uid,createdAt:now(),status:'READY'});
- return ref.id;
+const { db, now } = require('../lib/admin');
+const { assertRole } = require('../lib/auth');
+
+async function createDocumentRecord({
+  type,
+  recordId,
+  storagePath,
+  url = null,
+  auth
+}) {
+  assertRole(auth, [
+    'ADMIN',
+    'MANAGER',
+    'CUSTOMER_SERVICE'
+  ]);
+
+  const ref = db.collection('generatedDocuments').doc();
+
+  await ref.set({
+    type,
+    recordId,
+    storagePath: storagePath || null,
+    url,
+    createdBy: auth.uid,
+    createdAt: now(),
+    status: 'READY'
+  });
+
+  return ref.id;
 }
-module.exports={createDocumentRecord};
+
+module.exports = {
+  createDocumentRecord
+};
