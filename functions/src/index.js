fc9c64b8fdca4aad523aaff26be68ad56797a62e
trungythp1-jpg <trungythp1@gmail.com>
2026-09-21 17:17:39 +0700

    Update index.js

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,6 +2,10 @@ const { onRequest } = require('firebase-functions/v2/https');
 
 require('./lib/admin');
 
+const { bootstrapAdmin } = require('./auth/bootstrap-admin');
+
+exports.bootstrapAdmin = bootstrapAdmin;
+
 exports.health = onRequest((req, res) => {
   res.status(200).json({
     ok: true,
