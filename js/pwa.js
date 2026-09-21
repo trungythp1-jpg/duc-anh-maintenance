a96c3a8b5c9ffadbc7f016b0e5dfef1f215afee0
trungythp1-jpg <trungythp1@gmail.com>
2026-09-21 20:52:45 +0700

    Add files via upload

---
+++ js/pwa.js
@@ -0,0 +1,7 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js", { scope: "./" })
+      .then((registration) => console.log("PWA Service Worker registered:", registration.scope))
+      .catch((error) => console.error("PWA Service Worker registration failed:", error));
+  });
+}
