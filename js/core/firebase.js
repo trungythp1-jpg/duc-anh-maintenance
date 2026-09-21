c0cc1f359015c25c275e62348d9e8f97603fc138
trungythp1-jpg <trungythp1@gmail.com>
2026-09-21 19:56:19 +0700

    Update firebase.js

--- js/core/firebase.js
+++ js/core/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvctMMojc9qpbRiImmpFprc0RZ5Av00TU",
+  apiKey: "AIzaSyAvctMMojc9qpbRiImmpFprcORZ5Av00TU",
   authDomain: "duc-anh-maintenance-dev.firebaseapp.com",
   projectId: "duc-anh-maintenance-dev",
   storageBucket: "duc-anh-maintenance-dev.firebasestorage.app",
