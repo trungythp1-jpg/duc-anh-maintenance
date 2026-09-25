d265b27cae1868fc467b4afde55b05f6a028a5f2
trungythp1-jpg <trungythp1@gmail.com>
2026-09-25 19:36:43 +0700

    Update index.js

--- functions/src/cskh/index.js
+++ functions/src/cskh/index.js
@@ -1,3 +1,5 @@
+const { onCall } = require('firebase-functions/v2/https');
+
 const {
   createCSKHRequest,
   getCSKHRequest,
@@ -12,14 +14,34 @@ const {
   requestCSKHInfo
 } = require('./approval');
 
-module.exports = {
-  createCSKHRequest,
-  getCSKHRequest,
-  getCSKHRequests,
-  updateCSKHRequest,
-  submitCSKHRequest,
+exports.createCSKHRequest = onCall(async (request) => {
+  return createCSKHRequest(request);
+});
 
-  approveCSKHRequest,
-  rejectCSKHRequest,
-  requestCSKHInfo
-};
+exports.getCSKHRequest = onCall(async (request) => {
+  return getCSKHRequest(request);
+});
+
+exports.getCSKHRequests = onCall(async (request) => {
+  return getCSKHRequests(request);
+});
+
+exports.updateCSKHRequest = onCall(async (request) => {
+  return updateCSKHRequest(request);
+});
+
+exports.submitCSKHRequest = onCall(async (request) => {
+  return submitCSKHRequest(request);
+});
+
+exports.approveCSKHRequest = onCall(async (request) => {
+  return approveCSKHRequest(request);
+});
+
+exports.rejectCSKHRequest = onCall(async (request) => {
+  return rejectCSKHRequest(request);
+});
+
+exports.requestCSKHInfo = onCall(async (request) => {
+  return requestCSKHInfo(request);
+});
