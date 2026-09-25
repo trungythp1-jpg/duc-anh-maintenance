2a68af6a69a2ea03ed7de4507db2038b2eec572f
trungythp1-jpg <trungythp1@gmail.com>
2026-09-25 19:34:43 +0700

    Update index.js

--- functions/src/cskh/index.js
+++ functions/src/cskh/index.js
@@ -1,54 +1,25 @@
-const { onRequest } = require('firebase-functions/v2/https');
-
-require('./lib/admin');
-
-const { bootstrapAdmin } = require('./auth/bootstrap-admin');
+const {
+  createCSKHRequest,
+  getCSKHRequest,
+  getCSKHRequests,
+  updateCSKHRequest,
+  submitCSKHRequest
+} = require('./requests');
 
 const {
-  createWorkOrder,
-  completeWorkOrder,
-  closeWorkOrder
-} = require('./work-orders');
+  approveCSKHRequest,
+  rejectCSKHRequest,
+  requestCSKHInfo
+} = require('./approval');
 
-const {
-  requestConfirmation,
-  finalizeConfirmation
-} = require('./confirmations');
+module.exports = {
+  createCSKHRequest,
+  getCSKHRequest,
+  getCSKHRequests,
+  updateCSKHRequest,
+  submitCSKHRequest,
 
-const { processImport } = require('./imports');
-
-const cskh = require('./cskh');
-
-exports.health = onRequest((req, res) => {
-  res.status(200).json({
-    ok: true,
-    environment: 'DEV',
-    service: 'duc-anh-maintenance-functions',
-    version: 'V1.0'
-  });
-});
-
-exports.bootstrapAdmin = bootstrapAdmin;
-
-exports.createWorkOrder = createWorkOrder;
-exports.completeWorkOrder = completeWorkOrder;
-exports.closeWorkOrder = closeWorkOrder;
-
-exports.requestConfirmation = requestConfirmation;
-exports.finalizeConfirmation = finalizeConfirmation;
-
-exports.processImport = processImport;
-
-/* =========================
-   CSKH
-========================= */
-
-exports.createCSKHRequest = cskh.createCSKHRequest;
-exports.getCSKHRequest = cskh.getCSKHRequest;
-exports.getCSKHRequests = cskh.getCSKHRequests;
-exports.updateCSKHRequest = cskh.updateCSKHRequest;
-exports.submitCSKHRequest = cskh.submitCSKHRequest;
-
-exports.approveCSKHRequest = cskh.approveCSKHRequest;
-exports.rejectCSKHRequest = cskh.rejectCSKHRequest;
-exports.requestCSKHInfo = cskh.requestCSKHInfo;
+  approveCSKHRequest,
+  rejectCSKHRequest,
+  requestCSKHInfo
+};
